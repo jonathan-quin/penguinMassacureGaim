881a29c4105c9e457c2aa0d1f34871e9c7e77a7f
doegee <112405329+jonathan-quin@users.noreply.github.com>
2023-01-25 08:02:28 -0500

    fixed collisions in all directions

--- core/src/com/mygdx/game/scenes/testScene.java
+++ core/src/com/mygdx/game/scenes/testScene.java
@@ -16,9 +16,20 @@ public class testScene extends root {
 
         rootNode.addChild( new player(this,200,200) );
 
-        staticNode platform = new staticNode(this,400,400);
+        staticNode platform = new staticNode(this,100,400);
         platform.addChild(new collisionShape(128,128));
         rootNode.addChild(platform);
+
+        staticNode platform2 = new staticNode(this,430,400);
+        platform2.addChild(new collisionShape(128,128));
+        rootNode.addChild(platform2);
+
+        staticNode platform3 = new staticNode(this,400,300);
+        platform3.addChild(new collisionShape(128,128));
+        rootNode.addChild(platform3);
+
+
+
 
     }
 
