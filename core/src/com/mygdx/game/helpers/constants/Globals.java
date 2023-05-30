535237232705f6f302afb5954f000e8bad489a43
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2023-05-30 07:48:02 -0400

    ospaths

--- core/src/com/mygdx/game/helpers/constants/Globals.java
+++ core/src/com/mygdx/game/helpers/constants/Globals.java
@@ -83,15 +83,25 @@ public class Globals {
         sounds.put(MINIGUNSHOOT, Gdx.audio.newSound(Gdx.files.internal("sounds/miniGunShot.mp3")));
         sounds.put(REWINDSTATIC, Gdx.audio.newSound(Gdx.files.internal("sounds/staticcc.wav")));
 
-        soundPaths.put(ELFDIE, ( "assets/" + "sounds/elf explode.wav"));
-        soundPaths.put(BULLETSOFT, "assets/" + "sounds/bullet impact 1.wav");
-        soundPaths.put(BULLETHARD, "assets/" + "sounds/bullet impact 3.wav");
-        soundPaths.put(JUMP, "assets/" + "sounds/jump noisee.mp3");
-        soundPaths.put(SHOTGUNSHOOT, "assets/" + "sounds/shotgun shot.mp3");
-        soundPaths.put(REVOLVERSHOOT, "assets/" + "sounds/revolver shotFinal.mp3");
-        soundPaths.put(MINIGUNSHOOT, "assets/" + "sounds/miniGunShot.mp3");
-        soundPaths.put(REWINDSTATIC, "assets/" + "sounds/staticcc.wav");
+        String OSPath = "";
 
+        if (System.getProperty("os.name").equals("Windows 10")) OSPath = "assets/";
+
+        soundPaths.put(ELFDIE, ( OSPath + "sounds/elf explode.wav"));
+        soundPaths.put(BULLETSOFT, OSPath + "sounds/bullet impact 1.wav");
+        soundPaths.put(BULLETHARD, OSPath + "sounds/bullet impact 3.wav");
+        soundPaths.put(JUMP, OSPath + "sounds/jump noisee.mp3");
+        soundPaths.put(SHOTGUNSHOOT, OSPath + "sounds/shotgun shot.mp3");
+        soundPaths.put(REVOLVERSHOOT, OSPath + "sounds/revolver shotFinal.mp3");
+        soundPaths.put(MINIGUNSHOOT, OSPath + "sounds/miniGunShot.mp3");
+        soundPaths.put(REWINDSTATIC, OSPath + "sounds/staticcc.wav");
+
+    }
+
+    public String getOperatingSystem() {
+        String os = System.getProperty("os.name");
+        // System.out.println("Using System Property: " + os);
+        return os;
     }
 
     public static class Sounds{
